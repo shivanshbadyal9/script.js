157108f613fc47734d6e206d024e6111eb0923f9
shivanshbadyal9 <shivanshbadyal@gmail.com>
2026-07-16 10:36:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const text =
+"Jab se tum meri life me aayi ho, meri duniya aur bhi khoobsurat ho gayi hai. ❤️\n\nBabyyy I Love Uhh 😘💗\n\nTum meri smile ho, meri happiness ho aur meri favourite person ho. 🌹";
 
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 45);
+  }
+}
+
+window.onload = typeWriter;
+
+document.getElementById("openBtn").onclick = function () {
+  document.querySelector(".container").style.display = "none";
+  document.getElementById("proposal").style.display = "block";
+};
+
+document.getElementById("yesBtn").onclick = function () {
+  alert("💖 Yaaay!! ❤️\nForever Yours,\nShivansh 💍");
+};
